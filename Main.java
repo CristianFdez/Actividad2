7027a8f014f48ddd68184b283e7da42f79a2cafd
Guillermo Gabard <you@example.com>
2021-02-21 21:26:06 +0100

    Añado versión Main.java v1.1

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@ public class Main {
 		//Creamos los objetos
 		Operacion operacion = new Operacion(63, 23, 85.36, 51.35, 22);
 		
+		Producto pro1 = new Producto();
 		//Ejecutamos los metodos de prueba
 		System.out.println(operacion.obtenerPrimo());
+		System.out.println(pro1.pro2(12, 25));
+		
+	
 
 	}
 
