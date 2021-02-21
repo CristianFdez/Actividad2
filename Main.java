d17d87819829ee45163706a9c1a6fe0c3a1c6dfa
unknown <sorinpetrut.niculae@gmail.com>
2021-02-21 21:17:42 +0100

    Añado clase Main.java v1.0

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Creamos los objetos
+		Operacion operacion = new Operacion(63, 23, 85.36, 51.35, 22);
+		
+		//Ejecutamos los metodos de prueba
+		System.out.println(operacion.obtenerPrimo());
+
+	}
+
+}
