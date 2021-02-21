f739c75b489e0b66c6f8622c45112549bc24ec73
CristianFdez <cristian.titan95@gmail.com>
2021-02-21 21:38:17 +0100

    Añado la v1.3 de la clase Main

--- Main.java
+++ Main.java
@@ -9,11 +9,19 @@ public class Main {
 		
 		Cociente coc = new Cociente(6, 2);
 		
+		Resta resta = new Resta();
+		
 		//Ejecutamos los metodos de prueba
 		System.out.println(operacion.obtenerPrimo());
-		System.out.println(pro1.pro2(12, 25));
-		System.out.println(coc.divisionEnteros());
 		
+		System.out.println(pro1.pro2(12, 25));
+		
+		System.out.println(coc.divisionEnteros());
+
+		resta.restaAcumulado(5);
+		resta.restaAcumulado(2);
+		resta.restaAcumulado(4);
+		System.out.println(resta.getAcumulado());
 	
 
 	}
