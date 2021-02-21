6fcf3b26ac89dc9c26e94980d8230c4f856446be
Guillermo Gabard <you@example.com>
2021-02-21 19:27:30 +0100

    Añado v1.0 Clase producto

---
+++ Producto.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Clase producto calculadora actividad grupal 2
+ * Encontramos diferentes métodos donde podremos realizar las operaciones solicitadas. 
+ * Casos especiales: En el caso de que los numeros para realizar la potencia sean demasiado grandes
+ * como para ser procesador saldrá por consola un aviso de error. 
+ * 
+ * @see Producto
+ * @version 1.0
+ */
+public class Producto {
+	
+
+	
+	/**
+	 * Método en el que dando dos parametros (double) nos devuelve su producto. 
+	 * @param a Primer parametro para realizar la operación (debe ser un double)
+	 * @param b Segundo parametro para realizar la operación (debe ser un double)
+	 * @return  Producto de a y b
+	 */
+	
+	public double pro1 (double a , double b) {
+	return  a*b;
+	}
+	
+}
