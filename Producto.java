687649503f3d000cb453797643cc6cb081aca37a
Guillermo Gabard <you@example.com>
2021-02-21 19:30:00 +0100

    Añado v1.2 Clase producto

--- Producto.java
+++ Producto.java
@@ -35,5 +35,29 @@ public class Producto {
 		return a*b;	
 	}
 	
+	/**
+	 * Método en el que dando tres parametros (double) nos devuelve su producto. 
+	 * @param a Primer parametro para realizar la operación (debe ser un double)
+	 * @param b Segundo parametro para realizar la operación (debe ser un double)
+	 * @param c Tercer parametro para realizar la operación (debe ser un double)
+	 * @return  producto de a,b y c. 
+	 */
+
+	public double pro3( double a, double b, double c) {
+		return a*b*c;
+	}
+	
+	
+	/**
+	 * Método que nos devuelve un potencia dandole su base y cociente (ambos double)
+	 * @param base Pase Primer parametro para realizar la operación ( Debe ser la base y debe ser un double)
+	 * @param cociente  Cociente Segundo parametro para realizar la operación ( debe ser el cociente y debe ser un double)
+	 * @return Potencia resultante. 
+	 */
+	public double prod4 (double base, double cociente) {
+		return Math.pow ( base, cociente);
+	}
+}
+
 	
 }
