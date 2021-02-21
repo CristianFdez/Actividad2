25a841401efe6a9ff5454d6d7199b6f9443ddf09
amrobinska <78563946+amrobinska@users.noreply.github.com>
2021-02-21 21:32:23 +0100

    Añado version 1.2 del Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,13 @@ public class Main {
 		Operacion operacion = new Operacion(63, 23, 85.36, 51.35, 22);
 		
 		Producto pro1 = new Producto();
+		
+		Cociente coc = new Cociente(6, 2);
+		
 		//Ejecutamos los metodos de prueba
 		System.out.println(operacion.obtenerPrimo());
 		System.out.println(pro1.pro2(12, 25));
+		System.out.println(coc.divisionEnteros());
 		
 	
 
