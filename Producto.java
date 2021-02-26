32b0bbc955b9f9aefdbe3a2a52f47f35e462e77b
Guillermo Gabard <you@example.com>
2021-02-26 22:31:44 +0100

    Añado versión 1.4 para prueba

--- Producto.java
+++ Producto.java
@@ -16,23 +16,41 @@ public class Producto {
 	 * Método en el que dando dos parametros (double) nos devuelve su producto. 
 	 * @param a Primer parametro para realizar la operación (debe ser un double)
 	 * @param b Segundo parametro para realizar la operación (debe ser un double)
-	 * @return  Producto de a y b
+	 * Casos especiales:
+	 * -En el caso de que uno de los valores sean cero siempre dará como resultado cero
+	 * -En el caso de introducir un numero negativo dará como resultado -888.
+	 * 
 	 */
 	
-	public double pro1 (double a , double b) {
-	return  a*b;
-	}
+	public  void pro1 (double a , double b) {
+		if (a <0 || b <0)  
+			System.out.println("Error, -888");
+		
+		else 
+			System.out.println(a*b);
+		
+		}
+	
+	
 
 	/**
 	 * Método en el que dando dos parametros (int) nos devuelve su producto. 
 	 * @param a Primer parametro para realizar la operación (debe ser un int)
 	 * @param b Segundo parametro para realizar la operación (debe ser un int)
 	 * @return  Producto de a y b
+	 * Casos especiales:
+	 * -En el caso de que uno de los valores sean cero siempre dará como resultado cero
+	 * -En el caso de introducir un numero negativo dará como resultado -888.
 	 */
 	
 	
-	public int pro2 (int a, int b) {
-		return a*b;	
+	public void pro2 (int a, int b) {
+		if (a <0 || b <0)  
+			System.out.println("Error, -8888");
+		
+		else 
+			System.out.println(a*b);
+		
 	}
 	
 	/**
@@ -41,10 +59,17 @@ public class Producto {
 	 * @param b Segundo parametro para realizar la operación (debe ser un double)
 	 * @param c Tercer parametro para realizar la operación (debe ser un double)
 	 * @return  producto de a,b y c. 
+	 * Casos especiales:
+	 * -En el caso de que uno de los valores sean cero siempre dará como resultado cero
+	 * -En el caso de introducir un numero negativo dará como resultado -888.
 	 */
 
-	public double pro3( double a, double b, double c) {
-		return a*b*c;
+	public void pro3( double a, double b, double c) {
+		if (a <0 || b <0 || c<0)  
+			System.out.println("Error, -8888");
+		
+		else 
+			System.out.println(a*b*c);
 	}
 	
 	
@@ -53,9 +78,13 @@ public class Producto {
 	 * @param base Pase Primer parametro para realizar la operación ( Debe ser la base y debe ser un double)
 	 * @param cociente  Cociente Segundo parametro para realizar la operación ( debe ser el cociente y debe ser un double)
 	 * @return Potencia resultante. 
+	 * Casos especiales:
+	 * -Cualquier número diferente de cero elevado a la potencia cero es igual a uno. 
+	 * -Cero elevado a cualquier exponente positivo es igual a cero
+	 * -Cuando uno de los numeros es demasiado grande como para poder respresentarlo aparece "Infinity".
 	 */
-	public double prod4 (double base, double cociente) {
-		return Math.pow ( base, cociente);
+	public void prod4 (double base, double cociente) {
+		System.out.println(Math.pow ( base, cociente));
 	}
 }
 
